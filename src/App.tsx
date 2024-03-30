6b95798977a19e6bf8d09603974eea21cfcad1e3
Denis <bessonov94@gmail.com>
2024-03-30 19:48:28 +0300

    change webpack, ts and eslint confings

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.scss";
 import { Input } from "./components";
-import image from 'Images/react.png';
+import image from "./assets/react.png";
 
 const App = () => {
-    const [name, setName] = useState<string>('World');
-    return (
-        <main className="title__title-box">
-            <h1 className="title__name ">Hello {name}</h1>
-            <img src={image} alt="react-icon"/>
-            <Input
-                placeholder="World" 
-                onInput={(e) => {
-                    if (e.currentTarget.value) {
-                        setName(e.currentTarget.value);
-                    } else {
-                        setName('World');
-                    }
-                }}/>
-        </main>
-    );
-}
+  const [name, setName] = useState<string>("World");
+  return (
+    <main className="title__title-box">
+      <h1 className="title__name ">Hello {name}</h1>
+      <img src={image} alt="react-icon" />
+      <Input
+        placeholder="World"
+        onInput={(e) => {
+          if (e.currentTarget.value) {
+            setName(e.currentTarget.value);
+          } else {
+            setName("World");
+          }
+        }}
+      />
+    </main>
+  );
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,9 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-    entry: ["./src/index.tsx"],
+    entry: path.resolve(__dirname, 'src/index.tsx'),
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
-        alias: {
-          Images: path.resolve(__dirname, 'public/assets'),
-        },
     },
     output: {
         path: path.join(__dirname, "dist"),
@@ -29,16 +26,13 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name][ext]',
-                }
+                type: 'asset/resource'
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, "./public/index.html"),
+            template: path.join(__dirname, "public/index.html"),
         }),
         new MiniCssExtractPlugin({ filename: '[name].css' }),
         new ESLintPlugin({
@@ -48,4 +42,5 @@ module.exports = {
     devServer: {
         port: 3000,
     },
+    devtool: 'cheap-module-source-map'
 }
